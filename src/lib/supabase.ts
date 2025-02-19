06ac901362af3a4f5eace7a5b653fcca17cb27af
esttom <esttom42@gmail.com>
2025-02-20 02:03:00 +0900

    feat: add supabase chat

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,21 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 import type { Database } from './database.types'
 
-export class SupabaseProvider {
-  private static _instance: SupabaseProvider
-  private _client: ReturnType<typeof createClient<Database>>
-
-  private constructor() {
-    this._client = createClient(
-      import.meta.env.VITE_SUPABASE_URI,
-      import.meta.env.VITE_SUPABASE_ANON_KEY,
-    )
-  }
-
-  public static get instance() {
-    if (!this._instance) {
-      this._instance = new SupabaseProvider()
-    }
-    return this._instance
-  }
-}
+export const supabase = createClient<Database>(
+  import.meta.env.SUPABASE_URL,
+  import.meta.env.SUPABASE_ANON_KEY,
+);
